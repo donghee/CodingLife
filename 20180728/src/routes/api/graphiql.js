d75607d42a7f225e05a169ca96f2f908b7d1f08a
Donghee Park <dongheepark@gmail.com>
2018-08-08 01:59:01 +0900

    Rename graphql-server

--- 20180728/src/routes/api/graphiql.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import { graphiqlExpress } from 'graphql-server-express';
-
-export default graphiqlExpress({
-  endpointURL: '/api',
-  query: 'query Author \{\n  author(id: 1) {\n    firstName\n    lastName\n  }\n}'
-})
