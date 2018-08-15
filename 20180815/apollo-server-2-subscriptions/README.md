86737259aa199256d841a22b3be82d8fc6fc71bd
Donghee Park <dongheepark@gmail.com>
2018-08-15 17:08:46 +0900

    Change endpoint to /api/graphql

--- 20180815/apollo-server-2-subscriptions/README.md
+++ 20180815/apollo-server-2-subscriptions/README.md
@@ -18,4 +18,13 @@ apollo-server에httpServer Subscription Handlers 설치해야 apollo-server-expr
 server.installSubscriptionHandlers(httpServer);
 ```
 
+### apollo-server-express graphql url 수정
 
+graphql playground: http://localhost:4000/api/graphql
+
+graphql query mutation endpoint http://localhost:4000/api/graphql
+graphql subscription endpoint http://localhost:4000/api/graphql
+
+
+# 참고
+ - https://github.com/rwieruch/fullstack-apollo-subscription-example/blob/master/server/src/index.js
--- 20180815/apollo-server-2-subscriptions/server-express.js
+++ 20180815/apollo-server-2-subscriptions/server-express.js
@@ -1,6 +1,7 @@
 const { ApolloServer, gql, PubSub } = require('apollo-server-express');
 const http = require('http');
 const express = require('express');
+const { makeExecutableSchema } = require('graphql-tools');
 
 const pubsub = new PubSub();
 const SOMETHING_CHANGED_TOPIC = 'something_changed';
@@ -25,14 +26,33 @@ const resolvers = {
   },
 };
 
+
+// const server = new ApolloServer({
+//   schema: makeExecutableSchema({
+//     typeDefs: typeDefs,
+//     resolvers: resolvers
+//   })
+// });
+
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema: makeExecutableSchema({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+  }),
+  subscriptions: {
+    path: '/api/graphql',
+    onConnect: (connectionParams, webSocket, context) => {
+      console.log('connect...');
+    },
+    onDisconnect: (webSocket, context) => {
+      console.log('disconnect...');
+    },
+  },
 });
 
 var app = express();
-//server.applyMiddleware({ app });
-server.applyMiddleware({ app, path: '/' });
+// server.applyMiddleware({ app });
+server.applyMiddleware({ app, path: '/api/graphql' });
 
 //publish events every second
 setInterval(
@@ -48,4 +68,5 @@ server.installSubscriptionHandlers(httpServer);
 
 httpServer.listen({ port: 4000 }, () => {
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  console.log(`🚀 Subscriptions ready at ws://localhost:4000${server.subscriptionsPath}`);
 });
